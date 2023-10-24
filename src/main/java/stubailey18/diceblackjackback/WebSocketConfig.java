03d2f2ebac8e66abc19f3c130507def98c60d901
Stuart Bailey <stubailey18@hotmail.com>
2023-10-24 10:28:27 +0100

    Modified for SSL

--- src/main/java/stubailey18/diceblackjackback/WebSocketConfig.java
+++ src/main/java/stubailey18/diceblackjackback/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package stubailey18.diceblackjackback;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -9,9 +10,12 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    @Value("${allowed-origin}")
+    private String allowedOrigin;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
       registry.addHandler(new WebSocketHandler(), "/diceblackjack")
-              .setAllowedOrigins("https://affectionate-ride-7883fd.netlify.app");
+              .setAllowedOrigins(allowedOrigin);
     }
 }
